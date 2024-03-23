f6e4d2903e7df3210f2ebb199f30131d39b87b94
Anshul Dohare <anshuldohare6904@gmail.com>
2024-03-23 15:46:25 +0530

    SearchView Added, Toolbar Title change

--- app/src/main/java/com/example/roomdekho/dashboard/Home.java
+++ app/src/main/java/com/example/roomdekho/dashboard/Home.java
@@ -1,5 +1,6 @@
 package com.example.roomdekho.dashboard;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -8,11 +9,13 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.animation.LayoutTransition;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.roomdekho.R;
 import com.example.roomdekho.databinding.ActivityHomeBinding;
@@ -24,12 +27,15 @@ public class Home extends AppCompatActivity {
     private ActivityHomeBinding binding;
     Fragment fragment ;
     FragmentManager fragmentManager;
+    TextView actionBarName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        actionBarName = findViewById(R.id.action_bar_name);
+        actionBarName.setText("Rooms List");
 
         fragment = new RoomsList();
         fragmentManager = getSupportFragmentManager();
@@ -46,6 +52,7 @@ public class Home extends AppCompatActivity {
         drawerLayout.openDrawer(GravityCompat.START);
     }
     public void onProfileClicked(View view){
+        actionBarName.setText("Profile");
         String str = String.valueOf(FirebaseAuth.getInstance().getCurrentUser().getEmail());
         fragment = new Profile(str);
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -55,6 +62,7 @@ public class Home extends AppCompatActivity {
 
     }
     public  void onRoomsListClicked(View view){
+        actionBarName.setText("RoomsList");
         fragment = new RoomsList();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.linearlayout,fragment);
@@ -62,6 +70,7 @@ public class Home extends AppCompatActivity {
         binding.drawerLayout.closeDrawer(GravityCompat.START);
     }
     public void onAddRoomsClicked(View view){
+        actionBarName.setText("Add Rooms");
         fragment = new AddRooms();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.linearlayout,fragment);
@@ -69,8 +78,27 @@ public class Home extends AppCompatActivity {
         binding.drawerLayout.closeDrawer(GravityCompat.START);
     }
     public void onLogOutClicked(View view){
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(this, OpeningPage.class));
-        this.finish();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false).setTitle("RoomDekho").setIcon(R.mipmap.ic_launcher_foreground).setMessage("Are you want to logout ?").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                actionBarName.setText("Room Dekho");
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Home.this, OpeningPage.class));
+                Home.this.finish();
+            }
+        }).setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        }).show();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        binding.drawerLayout.closeDrawer(GravityCompat.START);
     }
 }
--- app/src/main/java/com/example/roomdekho/dashboard/RoomsList.java
+++ app/src/main/java/com/example/roomdekho/dashboard/RoomsList.java
@@ -1,10 +1,12 @@
 package com.example.roomdekho.dashboard;
 
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +14,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.roomdekho.R;
 import com.example.roomdekho.adapter.RoomInfo;
@@ -27,11 +32,14 @@ import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class RoomsList extends Fragment {
     RecyclerView recyclerView;
+    SearchView searchView;
     RoomListAdapter roomListAdapter;
     ArrayList<RoomInfo> roomsList;
+    ArrayList<RoomInfo> newRoomList;
     public RoomsList() {
         // Required empty public constructor
     }
@@ -47,12 +55,24 @@ public class RoomsList extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_rooms_list, container, false);
+        searchView = view.findViewById(R.id.searchRoom);
         recyclerView = view.findViewById(R.id.roomsList);
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(),1,GridLayoutManager.VERTICAL,false));
         recyclerView.setHasFixedSize(true);
         roomsList = new ArrayList<>();
+        newRoomList = new ArrayList<>();
         roomListAdapter = new RoomListAdapter(getContext(),roomsList);
         recyclerView.setAdapter(roomListAdapter);
+
+        View searchPlate = searchView.findViewById(androidx.appcompat.R.id.search_plate);
+        searchPlate.setBackgroundColor(getResources().getColor(R.color.aqua));
+        EditText searchText = searchView.findViewById(androidx.appcompat.R.id.search_src_text);
+        searchText.setTextColor(getResources().getColor(R.color.black));
+        ImageView searchIcon = searchView.findViewById(androidx.appcompat.R.id.search_button);
+        searchIcon.setColorFilter(getResources().getColor(R.color.black));
+        ImageView closeIcon = searchView.findViewById(androidx.appcompat.R.id.search_close_btn);
+        closeIcon.setColorFilter(getResources().getColor(R.color.white));
+
         FirebaseFirestore.getInstance().collection("AllRooms").addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
@@ -62,6 +82,34 @@ public class RoomsList extends Fragment {
                             documentSnapshot.getString("rRent"),documentSnapshot.getString("rDescription"), "RoomsPic/"+documentSnapshot.getString("rUserId")+"/"+documentSnapshot.getString("rRoomNumber")));
                 }
                 roomListAdapter.notifyDataSetChanged();
+            }
+        });
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if(!(query.isEmpty())){
+                    newRoomList.clear();
+                    String input = query.toLowerCase(Locale.ROOT);
+                    for(RoomInfo roomInfo : roomsList){
+                        if(roomInfo.getrCity().toLowerCase(Locale.ROOT).contains(input)||roomInfo.getrArea().toLowerCase(Locale.ROOT).contains(input)){
+                            newRoomList.add(roomInfo);
+                            Toast.makeText(getContext(), "Filter"+roomInfo.getrAddress(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    RoomListAdapter newRoomListAdapter = new RoomListAdapter(getContext(),newRoomList);
+                    recyclerView.setAdapter(newRoomListAdapter);
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(newText.isEmpty()){
+                    roomListAdapter = new RoomListAdapter(getContext(),roomsList);
+                    recyclerView.setAdapter(roomListAdapter);
+                    roomListAdapter.notifyDataSetChanged();
+                }
+                return false;
             }
         });
         return view;
